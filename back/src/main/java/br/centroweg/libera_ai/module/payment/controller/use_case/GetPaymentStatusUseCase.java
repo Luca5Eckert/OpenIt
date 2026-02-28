4fa4adc2914667a2e244e96d579dff6358f75003
Luca5Eckert <ecklukatv@gmail.com>
2026-02-28 00:51:44 -0300

    feat: add GetPaymentStatusUseCase

---
+++ back/src/main/java/br/centroweg/libera_ai/module/payment/controller/use_case/GetPaymentStatusUseCase.java
@@ -0,0 +1,22 @@
+package br.centroweg.libera_ai.module.payment.controller.use_case;
+
+import br.centroweg.libera_ai.module.payment.domain.model.Payment;
+import br.centroweg.libera_ai.module.payment.domain.port.PaymentRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GetPaymentStatusUseCase {
+
+    private final PaymentRepository paymentRepository;
+
+    public GetPaymentStatusUseCase(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
+    public boolean execute(String paymentId) {
+        return paymentRepository.findById(paymentId)
+                .map(Payment::isPaid)
+                .orElse(false);
+    }
+
+}
--- back/src/main/java/br/centroweg/libera_ai/module/payment/domain/port/PaymentRepository.java
+++ back/src/main/java/br/centroweg/libera_ai/module/payment/domain/port/PaymentRepository.java
@@ -2,7 +2,11 @@ package br.centroweg.libera_ai.module.payment.domain.port;
 
 import br.centroweg.libera_ai.module.payment.domain.model.Payment;
 
+import java.util.Optional;
+
 public interface PaymentRepository {
 
     void save(Payment payment);
+
+    Optional<Payment> findById(String paymentId);
 }
